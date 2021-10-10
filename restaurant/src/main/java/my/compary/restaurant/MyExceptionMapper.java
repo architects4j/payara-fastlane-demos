7b6e7f5e76eb3bd8329b97de6a8af6221c056b34
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-10 19:54:52 +0100

    creates validation provider

---
+++ restaurant/src/main/java/my/compary/restaurant/MyExceptionMapper.java
@@ -0,0 +1,28 @@
+package my.compary.restaurant;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class MyExceptionMapper
+        implements ExceptionMapper<ConstraintViolationException> {
+
+    @Override
+    public Response toResponse(final ConstraintViolationException exception) {
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(prepareMessage(exception))
+                .type("text/plain")
+                .build();
+    }
+
+    private String prepareMessage(ConstraintViolationException exception) {
+        StringBuilder msg = new StringBuilder();
+        for (ConstraintViolation<?> cv : exception.getConstraintViolations()) {
+            msg.append(cv.getPropertyPath() + " " + cv.getMessage() + "\n");
+        }
+        return msg.toString();
+    }
+}
