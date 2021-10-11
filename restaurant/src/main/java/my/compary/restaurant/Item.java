2f695afc44b03098de48150cd0a7732e0582f396
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-11 06:12:51 +0100

    fixes item validation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- restaurant/src/main/java/my/compary/restaurant/Item.java
+++ restaurant/src/main/java/my/compary/restaurant/Item.java
@@ -5,7 +5,6 @@ import my.compary.restaurant.infra.FieldPropertyVisibilityStrategy;
 
 import javax.json.bind.annotation.JsonbVisibility;
 import javax.validation.constraints.Future;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -33,7 +32,7 @@ public class Item {
     private LocalDate expires;
 
     @NotNull
-    @Min(value = 1, message = "There should be at least one ingredient")
+    @Size(min = 1, message = "There should be at least one ingredient")
     private List<Ingredient> ingredients;
 
 
