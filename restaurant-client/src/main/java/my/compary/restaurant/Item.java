e19fb9e136584389220cf27c941ebadcb550bf43
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-11 07:21:42 +0100

    remove open-api schema

--- restaurant-client/src/main/java/my/compary/restaurant/Item.java
+++ restaurant-client/src/main/java/my/compary/restaurant/Item.java
@@ -15,32 +15,17 @@ import java.util.List;
 import java.util.Objects;
 
 
-@Schema(name = "Item", description = "The entity that represents Item in a restaurant")
 @JsonbVisibility(FieldPropertyVisibilityStrategy.class)
 public class Item {
 
-    @Schema(required = true, name = "name", description = "The item name", example = "water")
-    @NotBlank
-    @Size(min = 3, max = 20, message = "The name size should be between 3 and 10 chars")
     private String name;
 
-    @Schema(required = true, name = "description", description = "The item description", example = "Water appears as a clear, nontoxic liquid composed of hydrogen and oxygen, essential for life.")
-    @NotBlank
-    @Size(min = 10, max = 100, message = "The description should be between 10 and 100 chars")
     private String description;
 
-    @Schema(required = true, name = "type", description = "The type name", example = "BEVERAGE")
-    @NotNull(message = "Fill up it with either BEVERAGE or FOOD")
     private ItemType type;
 
-    @Schema(required = true, name = "expires", description = "When the item expires", example = "2025-12-03")
-    @Future(message = "It is not possible to save an expired item")
-    @NotNull
     private LocalDate expires;
 
-    @NotNull
-    @Size(min = 1, message = "There should be at least one ingredient")
-    @Schema(required = true, name = "ingredients", description = "The ingredients")
     private List<Ingredient> ingredients;
 
     public String getName() {
