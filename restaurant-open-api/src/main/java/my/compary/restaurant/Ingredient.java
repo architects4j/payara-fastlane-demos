118004857cc7d91d0b1b959ac8528db2bbafff93
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-11 06:26:31 +0100

    defines ingredient as schema
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- restaurant-open-api/src/main/java/my/compary/restaurant/Ingredient.java
+++ restaurant-open-api/src/main/java/my/compary/restaurant/Ingredient.java
@@ -12,13 +12,16 @@ import javax.validation.constraints.Size;
 @JsonbVisibility(FieldPropertyVisibilityStrategy.class)
 public class Ingredient {
 
+    @Schema(required = true, name = "name", description = "The ingredient name", example = "water")
     @NotBlank
     @Size(min = 3, max = 20, message = "The name size should be between 3 and 10 chars")
     private String name;
 
+    @Schema(required = true, name = "unit", description = "The ingredient unit of measurement", example = "L")
     @NotBlank
     private String unit;
 
+    @Schema(required = true, name = "quantity", description = "The ingredient quantity", example = "1")
     @PositiveOrZero(message = "a quantity cannot be negative")
     private double quantity;
 
