45cc7bf6d9d906961696279c16d0eeed58a22af4
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-20 19:29:28 +0100

    creates TimedInterceptor class
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- cdi/src/main/java/my/compary/cdi/demo/auditaded/TimedInterceptor.java
+++ cdi/src/main/java/my/compary/cdi/demo/auditaded/TimedInterceptor.java
@@ -1,2 +1,18 @@
-package my.compary.cdi.demo.auditaded;public class TimedInterceptor {
+package my.compary.cdi.demo.auditaded;
+
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+
+@Timed
+@Interceptor
+public class TimedInterceptor {
+
+    @AroundInvoke
+    public Object auditMethod(InvocationContext ctx) throws Exception {
+        calledBefore = true;
+        Object result = ctx.proceed();
+        calledAfter = true;
+        return result;
+    }
 }
