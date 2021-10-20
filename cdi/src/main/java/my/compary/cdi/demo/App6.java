1f71c807c027c0b7f979aee10c5c7e6e59db455e
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-20 19:42:54 +0100

    creates app6 sample code

--- cdi/src/main/java/my/compary/cdi/demo/App6.java
+++ cdi/src/main/java/my/compary/cdi/demo/App6.java
@@ -1,17 +1,21 @@
 package my.compary.cdi.demo;
 
+import my.compary.cdi.demo.auditaded.FastSupplier;
+import my.compary.cdi.demo.auditaded.SlowSupplier;
 import my.compary.cdi.demo.decorator.Worker;
 
 import javax.enterprise.inject.se.SeContainer;
 import javax.enterprise.inject.se.SeContainerInitializer;
+import java.util.function.Supplier;
 
-public class App5 {
+public class App6 {
 
     public static void main(String[] args) {
         try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
-            Worker worker = container.select(Worker.class).get();
-            String work = worker.work("Just a single button");
-            System.out.println("The work result: " + work);
+            Supplier<String> fastSupplier = container.select(FastSupplier.class).get();
+            Supplier<String> slowSupplier = container.select(SlowSupplier.class).get();
+            System.out.println("The result: " + fastSupplier.get());
+            System.out.println("The result: " + slowSupplier.get());
         }
     }
 }
