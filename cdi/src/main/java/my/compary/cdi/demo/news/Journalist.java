df1403cb1fa6a7ed651f5f8bb11b811273ad3046
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-20 20:13:21 +0100

    Update journalist
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- cdi/src/main/java/my/compary/cdi/demo/news/Journalist.java
+++ cdi/src/main/java/my/compary/cdi/demo/news/Journalist.java
@@ -10,7 +10,15 @@ public class Journalist {
     @Inject
     private Event<News> event;
 
+    @Inject
+    @PrimeNews
+    private Event<News> primeEvent;
+
     public void receiveNews(News news) {
-         this.event.fire(news);
+        this.event.fire(news);
+    }
+
+    public void primeNews(News news) {
+        this.primeEvent.fire(news);
     }
 }
